ec4d31c2b99a162db3f80b6fbdb0186f2632854c
Muhammad Irshad <syyed.asshi89@gmail.com>
2022-10-27 16:12:58 +0500

    starts work on M-IV

---
+++ app.js
@@ -0,0 +1,397 @@
+/* Created and coded by Abhilash Narayan */
+/* Quiz source: w3schools.com */
+var quiz = {
+    "JS": [
+    {
+    "id": 1,
+    "question": "What is USP full form?",
+    "options": [
+    {
+    "a": "Unlimited selling point",
+    "b": "Unique showcase point.",
+    "c": "Unique selling Point",
+    "d": "Unique selling porpotional"
+    
+    }
+    ],
+    "answer": "Unique selling point",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 2,
+    "question": "USP can be define in one sentence",
+    "options": [
+    {
+    "a": "It is a worst part of a business",
+    "b": "It is important for marketing",
+    "c": "It is only use for single business",
+    "d": "it is use for internet advertisment only"
+    }
+    ],
+    "answer": "It is important for marketing",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 3,
+    "question": "Extraction of USP _______________",
+    "options": [
+    {
+    "a": "analyze the need of targated audience",
+    "b": "analyze market need",
+    "c": "analyze work load",
+    "d": "divide work in a huge team",
+    }
+    ],
+    "answer": "analyze the need of targated audience",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 4,
+    "question": "know your skills is the key point of Unique selling point?",
+    "options": [
+    {
+    "a": "True",
+    "b": "False"
+    }
+    ],
+    "answer": "True",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 5,
+    "question": "A logo is ___________ of the Brand",
+    "options": [
+    {
+    "a": "Need",
+    "b": "backbone",
+    "c": "structure",
+    "d": "identity",
+    }
+    ],
+    "answer": "identity",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 6,
+    "question": "A customer desire is ______________.",
+    "options": [
+    {
+    "a": "That's necessary of the product",
+    "b": "Related to the product",
+    "c": "What he wish to be in product but not necessary",
+    "d": "Both A & B"
+    }
+    ],
+    "answer": "Both A & B",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 7,
+    "question": "Choose incorrect option, Guidlines are necessary because______________",
+    "options": [
+    {
+    "a": "It is an easy guide to refer to when handing over the project",
+    "b": "When something awful happens, you can reffer them to the guidlines",
+    "c": "Contains documentation of product in a business",
+    "d": "Helps to make the design more solid"
+    }
+    ],
+    "answer": "When something awful happens, you can reffer them to the guidlines",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 8,
+    "question": "Choose incorrect option, A design layout should",
+    "options": [
+    {
+    "a": "Properly printed",
+    "b": "Represent USP",
+    "c": "Meet customer needs",
+    "d": " Balance"
+    }
+    ],
+    "answer": "Properly printed",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 9,
+    "question": "Choose incorrect option, Adobe photoshop & Corel draw are use for same purpose working.",
+    "options": [
+    {
+    "a": "True",
+    "b": "False"
+    }
+    ],
+    "answer": "False",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 10,
+    "question": "How many bars we can see in the Corel draw Interface at the start?",
+    "options": [
+    {
+    "a": "5",
+    "b": "2",
+    "c": "4",
+    "d": "6",
+    }
+    ],
+    "answer": "5",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 11,
+    "question": "Corel Draw is a Raster base software and works on pixels.",
+    "options": [
+    {
+    "a": "True",
+    "b": "False"
+    }
+    ],
+    "answer": "False",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 12,
+    "question": "Branding Guidlines means",
+    "options": [
+    {
+    "a": "client name, age, education",
+    "b": "size, color, theme",
+    "c": "unique idea",
+    "d": "Focus on USPs"
+    }
+    ],
+    "answer": "size, color, theme",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 13,
+    "question": "How to Create Target Personas with The Right Demographics",
+    "options": [
+    {
+    "a": "height, fair complexion, long hair",
+    "b": "age, gender, location",
+    "c": "identify by car or bike"
+    }
+    ],
+    "answer": "age, gender, location",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 14,
+    "question": "Why Unique selling point is important?",
+    "options": [
+    {
+    "a": "customer need",
+    "b": "strategy for attracting new customers",
+    "c": "market need "
+    }
+    ],
+    "answer": "strategy for attracting new customers",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 15,
+    "question": "Corel Draw is a :",
+    "options": [
+    {
+    "a": "Raster Software",
+    "b": "Tracing Software",
+    "c": "Bitmap Software",
+    "d": "Vector Software"
+    }
+    ],
+    "answer": "Vector Software",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 16,
+    "question": "How many bars we can find in Corel Draw Interface",
+    "options": [
+    {
+    "a": "4",
+    "b": "3",
+    "c": "5",
+    "d": "6"
+    }
+    ],
+    "answer": "5",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 17,
+    "question": "In corel Draw, shortcut key of pick tool is :",
+    "options": [
+    {
+    "a": "ctrl + spacebar",
+    "b": "ctrl + V",
+    "c": "V",
+    "d": "A"
+    }
+    ],
+    "answer": "ctrl + spacebar",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 18,
+    "question": "In corel Draw, shortcut key of Shape tool is :",
+    "options": [
+    {
+    "a": "F10",
+    "b": "F6",
+    "c": "F8",
+    "d": "F9"
+    }
+    ],
+    "answer": "F10",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 19,
+    "question": "Virtual Segment Delete Tool Use for :",
+    "options": [
+    {
+    "a": "cut unwanted part from an image",
+    "b": "divide object in 2 parts",
+    "c": "divide object in Multiple parts",
+    "d": "cut unwanted lines from an object"
+    }
+    ],
+    "answer": "cut unwanted lines from an object",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 20,
+    "question": "What fill tool would a person use if she/he wanted to fill an object with hearts?",
+    "options": [
+    {
+    "a": "Uniform",
+    "b": "Transparent",
+    "c": "Fountain",
+    "d": "Pattern",
+    }
+    ],
+    "answer": "Uniform",
+    "score": 0,
+    "status": ""
+    }
+    ]
+    }
+    var quizApp = function () {
+    this.score = 0;
+    this.qno = 1;
+    this.currentque = 0;
+    var totalque = quiz.JS.length;
+    this.displayQuiz = function (cque) {
+    this.currentque = cque;
+    if (this.currentque < totalque) {
+    $("#tque").html(totalque);
+    $("#previous").attr("disabled", false);
+    $("#next").attr("disabled", false);
+    $("#qid").html(quiz.JS[this.currentque].id + '.');
+    $("#question").html(quiz.JS[this.currentque].question);
+    $("#question-options").html("");
+    for (var key in quiz.JS[this.currentque].options[0]) {
+    if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+    $("#question-options").append(
+    "<div class='form-check option-block'>" +
+    "<label class='form-check-label'>" +
+    "<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+    quiz.JS[this.currentque].options[0][key] +
+    "</span></label>"
+    );
+    }
+    }
+    }
+    if (this.currentque <= 0) {
+    $("#previous").attr("disabled", true);
+    }
+    if (this.currentque >= totalque) {
+    $('#next').attr('disabled', true);
+    for (var i = 0; i < totalque; i++) {
+    this.score = this.score + quiz.JS[i].score;
+    }
+    return this.showResult(this.score);
+    }
+    }
+    this.showResult = function (scr) {
+    $("#result").addClass('result');
+    $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");
+    for (var j = 0; j < totalque; j++) {
+    var res;
+    if (quiz.JS[j].score == 0) {
+    res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+    } else {
+    res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+    }
+    $("#result").append(
+    '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+    '<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+    '<div class="last-row"><b>Score:</b> &nbsp;' + res +
+    '</div>'
+    );
+    }
+    }
+    this.checkAnswer = function (option) {
+    var answer = quiz.JS[this.currentque].answer;
+    option = option.replace(/</g, "&lt;") //for <
+    option = option.replace(/>/g, "&gt;") //for >
+    option = option.replace(/"/g, "&quot;")
+    if (option == quiz.JS[this.currentque].answer) {
+    if (quiz.JS[this.currentque].score == "") {
+    quiz.JS[this.currentque].score = 1;
+    quiz.JS[this.currentque].status = "correct";
+    }
+    } else {
+    quiz.JS[this.currentque].status = "wrong";
+    }
+    }
+    this.changeQuestion = function (cque) {
+    this.currentque = this.currentque + cque;
+    this.displayQuiz(this.currentque);
+    }
+    }
+    var jsq = new quizApp();
+    var selectedopt;
+    $(document).ready(function () {
+    jsq.displayQuiz(0);
+    $('#question-options').on('change', 'input[type=radio][name=option]', function (e) {
+    //var radio = $(this).find('input:radio');
+    $(this).prop("checked", true);
+    selectedopt = $(this).val();
+    });
+    });
+    $('#next').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(1);
+    });
+    $('#previous').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(-1);
+    });
